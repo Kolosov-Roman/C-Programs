aae2d17ae5421a3b77cd6dfd560ba0e1ee35b190
Roman <alekkolosov61@gmail.com>
2023-02-20 12:00:09 +0300

    Version 1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,50 @@
+﻿/*
+
+Console.WriteLine("Здравствуйте. Добро пожаловать в конвертер. Что хотите сконвертировать?\n" +
+    "\n" +
+    "1. Доллары в рубли\n" +
+    "2. Рубли в доллары\n" +
+    "\n" +
+    "Выберите 1 пункт из двух.\n" +
+    "");
+
+double c;
+double a;
+
+    c = Convert.ToDouble(Console.ReadLine());
+
+if (c == 1)
+    {
+        Console.WriteLine("\n" +
+            "Введите количество долларов.\n");
+        c = Convert.ToDouble(Console.ReadLine());
+        a = 56 * c;
+        Console.WriteLine(a + " рублей.");
+    }
+    else if (c == 2)
+    {
+        Console.WriteLine("\n" +
+            "Введите количество рублей.\n");
+        c = Convert.ToDouble(Console.ReadLine());
+        a = c / 56;
+        Console.WriteLine(a + " долларов.");
+    }
+    else
+{
+    Console.WriteLine("Выберите пункт 1 или 2.");
+ }
+
+*/
+
+int a;
+int b;
+a = int.Parse(Console.ReadLine());
+
+if (b == a / 2)
+{
+    Console.WriteLine("Число а - чётное.");
+}
+else
+{
+    Console.WriteLine("Число а - нечётное.");
+}
